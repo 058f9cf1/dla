91f77c0588fdb4bb3567756a62012f05378d895d
058f9cf1 <058f9cf1@tutanota.com>
2023-10-24 23:24:08 +0100

    Renamed some variables

--- dla.py
+++ dla.py
@@ -1,45 +1,46 @@
+#!/usr/bin/python
+
 import numpy
 import matplotlib.pyplot as plt
-import matplotlib.colors as colours
+from matplotlib.colors import ListedColormap as colours
 
 
-def create_matrix(r, s):
-	R_SQUARED = r ** 2
-	R_PLUS_S = r + s
-	BOX = R_PLUS_S * 2 + 1
+def create_matrix(r, p):
+	CENTRE = r + p
+	BOX = CENTRE * 2 + 1
 	
 	matrix = numpy.zeros((BOX, BOX))
-	matrix[R_PLUS_S][R_PLUS_S] = 1
+	matrix[CENTRE][CENTRE] = 1
 	
-	y, x = numpy.ogrid[-R_PLUS_S:BOX - R_PLUS_S, -R_PLUS_S:BOX - R_PLUS_S]
-	matrix[x ** 2 + y ** 2 >= R_SQUARED] = -1
+	y, x = numpy.ogrid[-CENTRE:CENTRE + 1, -CENTRE:CENTRE + 1]
+	matrix[x ** 2 + y ** 2 >= r ** 2] = -1
 	
 	return matrix
 
 
-def random_particle(r, s):
+def random_particle(r, p):
 	theta = 2 * numpy.pi * numpy.random.random()
-	x = int((r - 0.5) * (numpy.cos(theta) + 1)) + s + 1
-	y = int((r - 0.5) * (numpy.sin(theta) + 1)) + s + 1
+	x = int((r - 0.5) * (numpy.cos(theta) + 1)) + p + 1
+	y = int((r - 0.5) * (numpy.sin(theta) + 1)) + p + 1
 	return x, y
 
 
 def walk(x, y):
 	rand = numpy.random.random()
 	if(rand < 0.25):
-		return x, y - 1
+		return x, y - 1#Up
 	elif(rand < 0.5):
-		return x + 1, y
+		return x + 1, y#Right
 	elif(rand < 0.75):
-		return x, y + 1
-	return x - 1, y
+		return x, y + 1#Down
+	return x - 1, y#Left
 
 
-def run(m, r, s):
+def run(m, r, p):
 	complete = False
 	while not complete:
 		walking = True
-		x, y = random_particle(r, s)
+		x, y = random_particle(r, p)
 		while walking:
 			if(m[x][y] == -1):#If the particle has exited the circle
 				walking = False
@@ -53,9 +54,9 @@ def run(m, r, s):
 	return m
 
 if __name__ == "__main__":
-	radius = 199
-	space = 1
-	matrix = create_matrix(radius, space)
-	matrix = run(matrix, radius, space)
-	plt.matshow(matrix, cmap = colours.ListedColormap(['white', 'white', 'black']))
+	radius = 100
+	padding = 1 
+	matrix = create_matrix(radius, padding)
+	matrix = run(matrix, radius, padding)
+	plt.matshow(matrix, cmap = colours(['white', 'white', 'black']))
 	plt.show()
