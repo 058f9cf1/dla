c2df37c4231144ea5220474a482b073414fb5d38
058f9cf1 <058f9cf1@tutanota.com>
2026-02-07 20:22:00 +0000

    Optimised algorithm

--- dla.py
+++ dla.py
@@ -6,36 +6,37 @@ import matplotlib.colors as colours
 from matplotlib import colormaps as colourmaps
 
 
-def create_matrix(r_max, p):
-	CENTRE = r_max + p
-	BOX = CENTRE * 2 + 1
+def create_matrix(centre, r_max):
+	box = centre * 2 + 1
+
+	matrix = numpy.zeros((box, box))
+	matrix[centre][centre] = 1
 	
-	matrix = numpy.zeros((BOX, BOX))
-	matrix[CENTRE][CENTRE] = 1
-	
-	y, x = numpy.ogrid[-CENTRE:CENTRE + 1, -CENTRE:CENTRE + 1]
+	y, x = numpy.ogrid[-centre:centre + 1, -centre:centre + 1]
 	matrix[x ** 2 + y ** 2 >= r_max ** 2] = -1
 	
 	return matrix
 
 
-def random_particle(r_spawn, r_max, p):
+def spawn_particle(r_spawn, centre):
 	theta = 2 * numpy.pi * numpy.random.random()
-	x = int((r_spawn - 0.5) * numpy.cos(theta) + r_max + p + 0.5)
-	y = int((r_spawn - 0.5) * numpy.sin(theta) + r_max + p + 0.5)
+	x = int((r_spawn - 0.5) * numpy.cos(theta) + centre + 0.5)
+	y = int((r_spawn - 0.5) * numpy.sin(theta) + centre + 0.5)
 
 	return x, y
 
 
-def calculate_radius(x, y, r_spawn, r_max, p):
-	r = numpy.sqrt((x - r_max - p) ** 2 + (y - r_max - p) ** 2)
-	if r + 2 > r_spawn:
+def calculate_radii(r, r_spawn, r_kill, r_max):
+	if(r + 2 > r_spawn):
 		r_spawn = int(r) + 2
-		if r_spawn > r_max:
+		if(r_spawn > r_max):
 			r_spawn = r_max
-	print(f"Particle joined at ({x:>4}, {y:>4}), r = {r:<18}, r_spawn = {r_spawn}")
 
-	return r_spawn
+		r_kill = r_spawn * 10
+		if(r_kill > r_max):
+			r_kill = r_max
+
+	return r_spawn, r_kill
 
 
 def set_colours(name, mass):
@@ -49,47 +50,49 @@ def set_colours(name, mass):
 		for i in range(mass):
 			new_colourmap.append(numpy.array([0, 0, 0, 1]))
 
-	fixed = [
-			colours.to_rgba('lightgray'),	#Border
-			colours.to_rgba('white')		#Space
-			]
+	fixed = [colours.to_rgba('lightgrey'),	#Border
+			colours.to_rgba('white')]		#Space
 
 	return colours.ListedColormap(numpy.concatenate((fixed, new_colourmap)))
 
 
-def run(m, r_max, p):
+def generate(r_max, p):
 	complete = False
-	r_spawn = 2
+	r_spawn = 1
+	r_kill = r_spawn + 1
 	mass = 1
+
+	centre = r_max + p
+
+	m = create_matrix(centre, r_max)
 
 	while not complete:
 		walking = True
-		x, y = random_particle(r_spawn, r_max, p)
+		x, y = spawn_particle(r_spawn, centre)
 		while walking:
-			if(m[x][y] == -1):#If the particle has exited the circle
+			r = numpy.sqrt((x - centre) ** 2 + (y - centre) ** 2)
+			if(r > r_kill):#If the particle has exited the circle
 				walking = False
 			elif(m[x + 1][y] >= 1 or m[x - 1][y] >= 1 or m[x][y + 1] >= 1 or m[x][y - 1] >= 1):#If particle is touching the growth
-				m[x][y] = mass
 				mass += 1
+				m[x][y] = mass
 				walking = False
-				r_spawn = calculate_radius(x, y, r_spawn, r_max, p)
-				if(m[x + 1][y] == -1 or m[x - 1][y] == -1 or m[x][y + 1] == -1 or m[x][y - 1] == -1):#If the growth has reached the edge of the circle
+				r_spawn, r_kill = calculate_radii(r, r_spawn, r_kill, r_max)
+				print(f"Particle joined at ({x:>4}, {y:>4}), r = {r:<18}, r_spawn = {r_spawn}, r_kill = {r_kill}, mass = {mass}")
+				if(r + 1 > r_max):#If the growth has reached the edge of the circle
 					complete = True
-			else:
-				x, y = [x, y] + numpy.random.choice([-1, 0, 1], size=2)
+			else:#Walk
+				step = 1
+				if(r > r_spawn):
+					step = int(r) - r_spawn + 1
+				x, y = [x, y] + numpy.random.choice([-step, 0, step], size=2)
 
 	return m, mass
 
 
-def generate(r_max, p):
-	m = create_matrix(r_max, p)
-
-	return run(m, r_max, p)
-
-
 if __name__ == "__main__":
-	radius = 125
-	padding = 5
+	radius = 150
+	padding = 1
 	colourmap = "viridis"
 
 
@@ -98,12 +101,18 @@ if __name__ == "__main__":
 	print("Done generating!")
 
 	import os
-	print("Saving image")
+	os.makedirs("out", exist_ok=True)
+	path = os.path.join("out", f"dla_{radius}_{padding}_{mass}_{colourmap}")
+
+	print("Saving array data... ", end='')
+	numpy.save(f"{path}.npy", cluster)
+	print("Done!")
+
+	print("Saving image... ", end='')
 	plt.matshow(cluster, cmap=set_colours(colourmap, mass))
 	plt.axis('off')
-	os.makedirs("out", exist_ok=True)
-	path = os.path.join("out", f"dla_{radius}_{padding}_{colourmap}.png")
-	plt.savefig(path, bbox_inches='tight', dpi=500)
+	plt.savefig(f"{path}.png", bbox_inches='tight', dpi=500)
+	print("Done!")
 
 	print("Showing cluster")
 	plt.show()
